473df6e725a13f1f7ec1234cda1857091f81095a
Royston Shufflebotham <royston@shufflebotham.org>
2020-12-01 08:45:32 +0000

    2020 day 1

--- src/y2020/day01.rs
+++ src/y2020/day01.rs
@@ -10,6 +10,27 @@ pub fn parser(input: &str) -> Vec<i64> {
 }
 
 #[aoc(day1, part1)]
-pub fn day1_part1(data: &Vec<i64>) -> i64 {
-    0
+pub fn day1_part1(data: &Vec<i64>) -> Option<i64> {
+    for x in data {
+        for y in data {
+            if x + y == 2020 {
+                return Some(x * y);
+            }
+        }
+    }
+    None
+}
+
+#[aoc(day1, part2)]
+pub fn day1_part2(data: &Vec<i64>) -> Option<i64> {
+    for x in data {
+        for y in data {
+            for z in data {
+                if x + y + z == 2020 {
+                    return Some(x * y * z);
+                }
+            }
+        }
+    }
+    None
 }
